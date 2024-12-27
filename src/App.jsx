883db5954c52276722f0a86dff39c880233e7b98
RuslanGon <ruslan441006@gmail.com>
2024-12-27 12:24:11 +0200

    wer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import MailBox from "./components/MailBox.jsx";
+import meest from '../meest.json'
+import nova from '../nova.json'
+import ukr from '../ukr.json'
 
 function App() {
   return (
     <div>
-      <MailBox boxTitle="Meest Express" count={5}/>
-      <MailBox boxTitle="Nova Poshta" count={3}/>
-      <MailBox boxTitle="Ukr Poshta"/>
+      <MailBox boxUsers={meest} boxTitle="Meest Express" count={5}/>
+      <MailBox boxUsers={nova} boxTitle="Nova Poshta" count={3}/>
+      <MailBox boxUsers={ukr} boxTitle="Ukr Poshta"/>
     </div>
   );
 }
--- src/components/MailBox.jsx
+++ src/components/MailBox.jsx
@@ -1,21 +1,26 @@
-import css from './MailBox.module.css'
+import css from "./MailBox.module.css";
 
-const MailBox = ({boxTitle, count = 0}) => {
+const MailBox = ({ boxTitle, count = 0, boxUsers }) => {
   return (
     <div className={css.div}>
-        <h2 className={css.text}>{boxTitle}</h2>
-        {count === 0 ? (
+      <h2 className={css.text}>{boxTitle}</h2>
+      {count === 0 ? (
         <p>Сейчас нет активных отделений</p>
       ) : (
         <p>Количество активных отделений: {count}</p>
       )}
-        <ul>
-            <li>Ruslan@jmail.com</li>
-            <li>Lena@jmail.com</li>
-            <li>Igor@jmail.com</li>
-        </ul>
+      <ul>
+        {boxUsers.map((user) => {
+          return (
+            <div key={user.id}>
+              <li>Email: {user.userEmail}</li>
+              <li>Name: {user.userName}</li>
+            </div>
+          );
+        })}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default MailBox
+export default MailBox;
