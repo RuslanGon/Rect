9c2572edd64cb1e6fb704801e13360984edd90a6
RuslanGon <ruslan441006@gmail.com>
2024-12-28 13:34:33 +0200

    wer

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,22 @@ import DrinksCounter from "./components/DrinksCounter.jsx";
 import DrinksValues from "./components/DrinksValues.jsx";
 
 function App() {
-  const [drinks, setDrinks] = useState({ beer: 0, whiskey: 0, wine: 0 })
+  const [drinks, setDrinks] = useState({ beer: 0, whiskey: 0, wine: 0 });
 
   const handleLogDrink = (drinkName) => {
-    setDrinks({...drinks, [drinkName] : drinks[drinkName] + 1})
+    setDrinks({ ...drinks, [drinkName]: drinks[drinkName] + 1 });
     console.log(drinkName);
   };
 
- 
+  const reset = () => {
+    setDrinks({ beer: 0, whiskey: 0, wine: 0 });
+  };
 
   return (
     <div>
-      
       <DrinksValues drinks={drinks} />
       <DrinksCounter handleLogDrink={handleLogDrink} />
+      <button onClick={reset}>Recet</button>
       {/* <MailBox boxUsers={meest} boxTitle="Meest Express" count={5}/>
       <MailBox boxUsers={nova} boxTitle="Nova Poshta" count={3}/>
       <MailBox boxUsers={ukr} boxTitle="Ukr Poshta"/> */}
