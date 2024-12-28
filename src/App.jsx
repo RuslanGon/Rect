592d7a39ed5cd85113b7ab00c712441c56b86b6a
RuslanGon <ruslan441006@gmail.com>
2024-12-28 12:32:03 +0200

    wer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import "./App.css";
-import MailBox from "./components/MailBox.jsx";
-import meest from '../meest.json'
-import nova from '../nova.json'
-import ukr from '../ukr.json'
+// import MailBox from "./components/MailBox.jsx";
+// import meest from '../meest.json'
+// import nova from '../nova.json'
+// import ukr from '../ukr.json'
+import DrinksCounter from "./components/DrinksCounter.jsx";
+import DrinksValues from "./components/DrinksValues.jsx";
 
 function App() {
+
+  const handleLogDrink = (drinkName) => {
+    console.log(drinkName);
+  };
+
   return (
     <div>
-      <MailBox boxUsers={meest} boxTitle="Meest Express" count={5}/>
+      <DrinksValues drinks={{beer: 3, whiskey: 5, wine: 1}}/>
+      <DrinksCounter handleLogDrink={handleLogDrink} />
+      {/* <MailBox boxUsers={meest} boxTitle="Meest Express" count={5}/>
       <MailBox boxUsers={nova} boxTitle="Nova Poshta" count={3}/>
-      <MailBox boxUsers={ukr} boxTitle="Ukr Poshta"/>
+      <MailBox boxUsers={ukr} boxTitle="Ukr Poshta"/> */}
+   
     </div>
   );
 }
---
+++ src/components/DrinksValues.jsx
@@ -0,0 +1,13 @@
+
+
+const DrinksValues = ({drinks}) => {
+  return (
+<ul>
+    <li>Beer: {drinks.beer}</li>
+    <li>Whiskey: {drinks.whiskey}</li>
+    <li>Wine: {drinks.wine}</li>
+</ul>
+  )
+}
+
+export default DrinksValues
