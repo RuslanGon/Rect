920423f39d82110ee75307bb2b149405d5e6f1b6
RuslanGon <ruslan441006@gmail.com>
2025-01-05 14:25:13 +0200

    wer

--- src/AppHTTPrequests.jsx
+++ src/AppHTTPrequests.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import css from './HTTP.module.css'
 import Loader from "./components/Loader.jsx";
+import CartList from "./components/CartList.jsx";
 
 const AppHTTPrequests = () => {
   const [carts, setCarts] = useState([]);
@@ -27,25 +28,7 @@ const AppHTTPrequests = () => {
     <div className={css.div}>
       <h1 className={css.title}>Cars from USA</h1>
       {isLoading && <Loader />}
-      <ul >
-        {carts.map((cart) => (
-          <li key={cart.id}>
-            <ul className={css.list}>
-              {cart.products.map((product) => (
-                <li key={product.id}>
-                  <h3>Title: {product.title}</h3>
-                  <p>Price: ${product.price}</p>
-                  <p>Quantity: {product.quantity}</p>
-                  <h4>Total: ${product.total}</h4>
-                  <p>Discount: {product.discountPercentage}%</p>
-                  <p>Discounted Total: ${product.discountedTotal}</p>
-                  <img src={product.thumbnail} alt={product.title} />
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
+      <CartList carts={carts} />
     </div>
   );
 };
