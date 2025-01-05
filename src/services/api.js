f3392193b2f586b698b2145d3fff4337c39154d6
RuslanGon <ruslan441006@gmail.com>
2025-01-05 17:14:49 +0200

    wer

--- src/services/api.js
+++ src/services/api.js
@@ -4,3 +4,10 @@ export const requestCarts = async () => {
   const { data } = await axios.get("https://dummyjson.com/carts");
   return data;
 };
+
+export const requestCartsByQuery = async (query = '') => {
+    const { data } = await axios.get(`https://dummyjson.com/products/search?q=${query}`);
+    return data;
+  };
+
+
