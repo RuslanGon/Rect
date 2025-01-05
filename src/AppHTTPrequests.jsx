7fff2bdf646cfbb341428b759182226dc4e600e5
RuslanGon <ruslan441006@gmail.com>
2025-01-05 14:15:22 +0200

    wer

--- src/AppHTTPrequests.jsx
+++ src/AppHTTPrequests.jsx
@@ -1,18 +1,23 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import css from './HTTP.module.css'
+import Loader from "./components/Loader.jsx";
 
 const AppHTTPrequests = () => {
   const [carts, setCarts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
     async function fetchCarts() {
       try {
+        setIsLoading(true)
         const { data } = await axios.get("https://dummyjson.com/carts");
         console.log(data);
         setCarts(data.carts);
       } catch (error) {
         console.error("Error fetching carts:", error);
+      } finally {
+        setIsLoading(false)
       }
     }
     fetchCarts();
@@ -21,6 +26,7 @@ const AppHTTPrequests = () => {
   return (
     <div className={css.div}>
       <h1 className={css.title}>Cars from USA</h1>
+      {isLoading && <Loader />}
       <ul >
         {carts.map((cart) => (
           <li key={cart.id}>
