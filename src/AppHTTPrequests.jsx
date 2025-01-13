928d346f803d5fc47a89a321fdd69d8c85c20483
RuslanGon <ruslan441006@gmail.com>
2025-01-13 11:16:06 +0200

    wer

--- src/AppHTTPrequests.jsx
+++ src/AppHTTPrequests.jsx
@@ -1,35 +1,26 @@
-// {
-//   "id": 1,
-//   "title": "Essence Mascara Lash Princess",
-//   "description": "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects. Achieve dramatic lashes with this long-lasting and cruelty-free formula.",
-//   "category": "beauty",
-//   "price": 9.99,
-//   "discountPercentage": 7.17,
-//   "rating": 4.94,
-//   "stock": 5,
-//   "tags": [
-//     "beauty",
-//     "mascara"
-//   ],
-//   "brand": "Essence",
-//   "sku": "RCH45Q1A",
-//   "weight": 2,
-//   "dimensions": {
-//     "width": 23.17,
-//     "height": 14.43,
-//     "depth": 28.01
-
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Loader from "./components/Loader.jsx";
+import Error from "./components/Error.jsx";
 
 const AppHTTPrequests = () => {
   const [products, setProducts] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     async function fetchProducts() {
-      const { data } = await axios.get("https://dummyjson.com/products");
-      console.log(data);
-      setProducts(data.products)
+      setIsLoading(true); 
+      try {
+        const { data } = await axios.get("https://dummyjson.com/products");
+        console.log(data);
+        setProducts(data.products);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
     }
     fetchProducts();
   }, []);
@@ -37,16 +28,26 @@ const AppHTTPrequests = () => {
   return (
     <div>
       <h1>Super market</h1>
+      {isLoading && <Loader />}
+      {isError && <Error />}
       <ul>
-        {Array.isArray(products) && products.map(product => {
-          return (<li key={product.id}>
-            <img width={200} height={200} src={product.images} alt={product.title} />
-            <h2>Title:{product.title}</h2>
-            <p>Brand: {product.brand}</p>
-            <p>Rating: {product.rating}</p>
-            <h3>Price: {product.price}</h3>
-          </li>)
-        }) }
+        {Array.isArray(products) &&
+          products.map((product) => {
+            return (
+              <li key={product.id}>
+                <img
+                  width={200}
+                  height={200}
+                  src={product.images}
+                  alt={product.title}
+                />
+                <h2>Title:{product.title}</h2>
+                <p>Brand: {product.brand}</p>
+                <p>Rating: {product.rating}</p>
+                <h3>Price: {product.price}</h3>
+              </li>
+            );
+          })}
       </ul>
     </div>
   );
