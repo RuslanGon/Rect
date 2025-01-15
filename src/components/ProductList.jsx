69e0ad91d3c0678a9f5fe062cd391b7fb1e08389
RuslanGon <ruslan441006@gmail.com>
2025-01-15 13:49:32 +0200

    wer

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -14,7 +14,7 @@ const ProductList = ({ products }) => {
                 src={product.images[0]} // Первое изображение из массива
                 alt={product.title}
               />
-              <h2>Title: {product.title}</h2>
+              <h2>{product.title}</h2>
               <p><strong>Brand:</strong> {product.brand}</p>
               <p><strong>Category:</strong> {product.category}</p>
               <p><strong>Rating:</strong> {product.rating}</p>
