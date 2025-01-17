ad738b5dcb3913542e33734c030f7cc7c9c1988f
RuslanGon <ruslan441006@gmail.com>
2025-01-17 16:21:51 +0200

    wer

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,9 +1,24 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, Navigate } from "react-router-dom";
 
 const NotFoundPage = () => {
+  const [timer, setTimer] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTimer((prevTimer) => prevTimer + 1);
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  if (timer === 5) return <Navigate to="/" replace />;
+
   return (
     <div style={{ textAlign: "center", padding: "40px", color: "#333" }}>
-      <h1 style={{ fontSize: "36px" }}> Oops! The page you are looking for doesnt exist.</h1>
+      <h1 style={{ fontSize: "36px" }}>
+        Oops! The page you are looking for doesnt exist.
+      </h1>
+      <h2>You will be redirected to home in {5 - timer} seconds</h2>
       <Link to="/">Go to Home page</Link>
     </div>
   );
