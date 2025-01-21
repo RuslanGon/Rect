606ca3a1524882daa4bff1b278b5392bc080a480
RuslanGon <ruslan441006@gmail.com>
2025-01-21 18:53:31 +0200

    wer

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import AppRouter from './AppRouter.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.js'
+// import { persistor, store } from './redux/store.js'
+// import { PersistGate } from 'redux-persist/integration/react'
 // import AppHTTPCars from './AppHTTPCars.jsx'
 // import AppHTTPrequests from './AppHTTPrequests.jsx'
 // import App from './App.jsx'
@@ -14,11 +16,13 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
+      {/* <PersistGate persistor={persistor}> */}
         {/* <App /> */}
         {/* <AppDrinks /> */}
         {/* <AppHTTPrequests /> */}
         {/* <AppHTTPCars /> */}
         <AppRouter />
+        {/* </PersistGate> */}
       </BrowserRouter>
     </Provider>
   </StrictMode>
