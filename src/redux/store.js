b4219a3c90906d923eb35dd9218e592cd95aaccf
RuslanGon <ruslan441006@gmail.com>
2025-01-21 17:26:32 +0200

    wer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
-import {combineReducers, createStore} from 'redux'
-import { mailboxReducer } from './mailbox/mailboxReducer.js'
 
-const rootReducer = combineReducers({
-mailbox: mailboxReducer
+import { configureStore } from "@reduxjs/toolkit";
+import { mailboxReducer } from "./mailbox/mailboxReducer.js";
+
+export const store = configureStore({
+    reducer: {
+        mailbox: mailboxReducer
+    }
 })
-
-export const store = createStore(rootReducer)
