94a0ad9e044e5e4fc62ccd2db728802f1331aa9b
RuslanGon <ruslan441006@gmail.com>
2025-01-25 12:24:24 +0200

    wer

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,49 @@
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as Yup from "yup";
+
+const initialValues = {
+  email: "",
+  password: ""
+};
+
+const loginSchema = Yup.object().shape({
+  email: Yup.string().email("Must be a valid email!").required("Email is required"),
+  password: Yup.string().required("Password is required"),
+});
 
 const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
-}
+  const handleSubmit = (values, actions) => {
+    console.log(values);
+    actions.resetForm();
+  };
 
-export default LoginPage
+  return (
+    <Formik 
+      initialValues={initialValues} 
+      onSubmit={handleSubmit} 
+      validationSchema={loginSchema}
+    >
+      <Form>
+        <h2>Login User</h2>
+        <br />
+        <label>
+          <span>Email:</span>
+          <br />
+          <Field type="email" name="email" placeholder="Enter your email" />
+          <ErrorMessage name="email" component="span" style={{ color: "red" }} />
+        </label>
+        <br />
+        <label>
+          <span>Password:</span>
+          <br />
+          <Field type="text" name="password" placeholder="Enter your password" />
+          <ErrorMessage name="password" component="span" style={{ color: "red" }} />
+        </label>
+        <br />
+        <button type="submit">Register New User 👱</button>
+      </Form>
+    </Formik>
+  );
+};
+
+export default LoginPage;
--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -1,8 +1,58 @@
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as Yup from "yup";
 
-const RegistrationPage  = () => {
+const initialValues = {
+  name: "",
+  email: "",
+  password: ""
+};
+
+const registorSchema = Yup.object().shape({
+  name: Yup.string().required("Name is required"),
+  email: Yup.string().email("Must be a valid email!").required("Email is required"),
+  password: Yup.string().required("Password is required"),
+});
+
+const RegistrationPage = () => {
+  const handleSubmit = (values, actions) => {
+    console.log(values);
+    actions.resetForm();
+  };
+
   return (
-    <div>RegistrationPage </div>
-  )
-}
+    <Formik 
+      initialValues={initialValues} 
+      onSubmit={handleSubmit} 
+      validationSchema={registorSchema}
+    >
+      <Form>
+        <h2>Register new User</h2>
+        <br />
+        <label>
+          <span>Name:</span>
+          <br />
+          <Field type="text" name="name" placeholder="Enter your name" />
+          <ErrorMessage name="name" component="span" style={{ color: "red" }} />
+        </label>
+        <br />
+        <label>
+          <span>Email:</span>
+          <br />
+          <Field type="email" name="email" placeholder="Enter your email" />
+          <ErrorMessage name="email" component="span" style={{ color: "red" }} />
+        </label>
+        <br />
+        <label>
+          <span>Password:</span>
+          <br />
+          <Field type="text" name="password" placeholder="Enter your password" />
+          <ErrorMessage name="password" component="span" style={{ color: "red" }} />
+        </label>
+        <br />
+        <button type="submit">Register New User 👱</button>
+      </Form>
+    </Formik>
+  );
+};
 
-export default RegistrationPage 
+export default RegistrationPage;
